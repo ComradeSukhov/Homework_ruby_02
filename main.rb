8f19bde211064d29dac9c1d71ea9688be7d0d1e8
Dmitry Samoilov <sukhov@localhost.localdomain>
2020-09-27 13:16:31 +0300

    All functions are implemented Code is commented

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
-require 'csv'
-require_relative 'class.rb'
+require_relative 'modules.rb'
 
-Pc.most_expensive
-Pc.cheapest
+# Pc.most_expensive(10)
+# Pc.cheapest(10)
+# Pc.largest_by_type(10, 'sata')
+# Pc.most_additional_disks_attached_quantity(10)
+Pc.most_additional_disks_attached_volume(10)
