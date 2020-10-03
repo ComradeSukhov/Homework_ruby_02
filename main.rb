305ef0e8181b79fe7dd6988a2b40fe1a07033c43
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-03 12:10:32 +0300

    Edited the title text

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
 require_relative 'classes.rb'
 
-STDOUT.puts '10 самых дорогих ВМ'
+STDOUT.puts "10 самых дорогих ВМ \n"
 Pc.most_expensive(10)
 
-STDOUT.puts '10 самых дешевых ВМ'
+STDOUT.puts "10 самых дешевых ВМ \n"
 Pc.cheapest(10)
 
-STDOUT.puts '10 самых объемных по параметру type ВМ'
+STDOUT.puts "10 самых объемных по параметру type ВМ. Type = sata \n"
 Pc.largest_by_type(10, 'sata')
 
-STDOUT.puts '10 ВМ у которых подключено больше всего дополнительных дисков по количеству'
+STDOUT.puts "10 ВМ у которых подключено больше всего дополнительных дисков по количеству \n"
 Pc.most_additional_disks_attached_quantity(10)
 
-STDOUT.puts '10 ВМ у которых подключено больше всего дополнительных дисков (по объему)'
+STDOUT.puts "10 ВМ у которых подключено больше всего дополнительных дисков по объему \n"
 Pc.most_additional_disks_attached_volume(10)
